ee6b1fea14419c7312509d2033a35eae013fc48d
RidloGhifary <ridloghfry@gmail.com>
2024-03-18 15:02:29 +0700

    local variable

--- 1.php
+++ 1.php
@@ -6,9 +6,17 @@ MULTI
 LINE 
 COMMENTS  -->
 
-<?= 
+<?php
   $str = "John doe";
   $age = 25;
 
   echo "my name is $str and iam $age years old";
+
+  function myFunc(){
+    $name = "random";
+    echo "this is from inside function $name\n";
+  };
+
+  myFunc();
+  // echo "this line wont be executed because of local variable $name"
 ?>
