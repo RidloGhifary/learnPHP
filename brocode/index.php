c9c930ac7322cf442cc4dd4aadb3f907f4ba4083
RidloGhifary <ridloghfry@gmail.com>
2024-03-20 14:38:32 +0700

    username & password

---
+++ brocode/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="index.php" method="post">
+    <label for="username">username:</label>
+    <input type="text" name="username" id="username">
+    <label for="password">password:</label>
+    <input type="password" name="password" id="password">
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
+
+<?php 
+  echo "{$_POST['username']} <br/>";
+  echo "{$_POST['password']} <br/>";
+?>
