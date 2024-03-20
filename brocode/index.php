1b483ca6f673bbb1f4f804d761d4091cb3f70f31
RidloGhifary <ridloghfry@gmail.com>
2024-03-20 18:39:53 +0700

    cookie

--- brocode/index.php
+++ brocode/index.php
@@ -1,13 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <?php include("header.html") ?>
-  <p>this is a home page <br/> stuff about home page can go here</p>
-  <?php include("footer.html") ?>
-</body>
-</html>
+<?php 
+
+  setcookie("fav_food","pizza",time() + (86400 * 2), "/");
+  setcookie("fav_drink","coca cola",time() + (86400 * 3), "/");
+  setcookie("fav_game","valorant",time() - 0, "/");
+
+  
+  foreach($_COOKIE as $key => $value){
+    echo "{$key} = {$value} <br/>";
+  }
+
+  if(isset($_COOKIE['fav_game'])){
+    echo "I love playing {$$_COOKIE['fav_game']}";
+  } else{
+    echo "I don`t like playing game";
+  }
+?>
+
