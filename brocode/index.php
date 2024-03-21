80c91a69c6b1a09e5d3e023563fdd7b9bd696cc2
RidloGhifary <ridloghfry@gmail.com>
2024-03-21 12:53:56 +0700

    insert data to phpmyadmin sql database

--- brocode/index.php
+++ brocode/index.php
@@ -1,15 +1,19 @@
 <?php
-  include("database.php")
-?>
+  include("database.php");
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  Hello<br/>
-</body>
-</html>
+  $username = 'test';
+  $password = 'test';
+
+  $hash_password = password_hash($password, PASSWORD_DEFAULT);
+
+  $sql = "INSERT INTO users (user, password) VALUES ('$username', '$hash_password')";
+
+  try{
+    mysqli_query($conn, $sql);
+    echo "register successful";
+  } catch(mysqli_sql_exception){
+    echo "cannot register user";
+  }
+
+  mysqli_close($conn);
+?>
