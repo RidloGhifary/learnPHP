96e2fb54281f9a62d7c28809ba846d8ec92a9ffe
RidloGhifary <ridloghfry@gmail.com>
2024-03-27 15:49:39 +0700

    touched accidently

--- loginRegister/database.php
+++ loginRegister/database.php
@@ -1,16 +1,15 @@
 <?php
 
-  $db_server = "localhost";
-  $db_host = "root";
-  $db_password = "";
-  $db_name = "learnphplogin";
-  $connection = "";
+$db_server = "localhost";
+$db_host = "root";
+$db_password = "";
+$db_name = "learnphplogin";
+$connection = "";
 
-  try{
-    $connection = mysqli_connect($db_server, $db_host, $db_password, $db_name);
-  } catch(mysqli_sql_exception){
-    echo "connection error";
-  }
+try {
+  $connection = mysqli_connect($db_server, $db_host, $db_password, $db_name);
+} catch (mysqli_sql_exception) {
+  echo "connection error";
+}
 
-  if(!$connection) echo "connection error";
-?>
+if (!$connection) echo "connection error";
